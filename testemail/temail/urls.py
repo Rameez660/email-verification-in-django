05c811bb56ab32fee43c19a9ad0cf711b0c848b9
Muhammad Rameez <55558397+Rameez660@users.noreply.github.com>
2020-09-05 23:44:40 +0500

    Update urls.py

--- testemail/temail/urls.py
+++ testemail/temail/urls.py
@@ -5,7 +5,7 @@ from django.urls import path,re_path
 urlpatterns = [
     # path('', views.signup),
     path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
+    path('login/',views.handlelogin,name="handlelogin"),
     path('student/', views.student, name='student'),
 
     # url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
