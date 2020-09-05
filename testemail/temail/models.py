5910c50d9b1cdf58fb8f73038018d663323c2a58
Rameez660 <muhammadrameez660@gmail.com>
2020-09-05 15:12:52 +0500

    first commit

---
+++ testemail/temail/models.py
@@ -0,0 +1,8 @@
+
+from django.db import models
+
+# Create your models here.
+# class UserData(models.Model):
+#     email = models.EmailField(max_length=200, help_text='Required')
+#     name = models.CharField(max_length=200)
+#     password= models.CharField(max_length=200)
